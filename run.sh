18bc5e03ba987e72df0203f863e9ee07f1d572c9
sms <s.m.scarano@gmail.com>
2024-04-03 08:44:10 -0300

    added python deps install, echoes to script

--- run.sh
+++ run.sh
@@ -6,9 +6,16 @@ repoPath="/home/sms/code/removedor_marcas_tiempo"
 
 # Navigate to the repository directory
 cd "$repoPath"
-echo pwd
+echo "working on $repoPath directory"
+
+# Install python script dependencies
+echo "Install python script dependencies"
+pip install -r requirements.txt
+
+
 
 # Perform a Git fetch to update the remote references
+echo "Perform a Git fetch to update the remote references"
 git fetch
 
 # Check if there are any updates available from the remote repository
@@ -16,8 +23,11 @@ updatesAvailable=$(git status -uno)
 
 # If updates are available, pull the changes from the remote repository
 if [[ $updatesAvailable == *"Your branch is behind"* ]]; then
+    # Perform a Git fetch to update the remote references
+    echo "updates are available, pull the changes from the remote repository"
     git pull
 fi
 
 # Run the Python script from the repository directory
+echo "Running python script"
 python3 main.py
